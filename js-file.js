0de6fd10e1d76673e03715180a7911b615d079bc
somnif <teewaard@gmail.com>
2024-05-16 16:33:23 +1200

    Add base html files, and initailize grid through .js

---
+++ js-file.js
@@ -0,0 +1,23 @@
+const container = document.querySelector("#container")
+const INITIAL_SIZE = 16;
+let gridWidth = INITIAL_SIZE;
+let gridHeight = INITIAL_SIZE;
+
+const drawGrid = (width, height) => {
+    container.innerText = "";
+    for (let i = 0; i < height; i++){
+        let row = document.createElement("div")
+        row.setAttribute("class", "grid-row")
+
+        for (let j = 0; j < width; j++){
+            let grid = document.createElement("div")
+            grid.setAttribute("class", "grid-item")
+            row.appendChild(grid)
+        }
+        container.appendChild(row)
+    }
+}
+
+window.addEventListener("load", drawGrid(gridWidth, gridHeight), {
+    once: true
+});
