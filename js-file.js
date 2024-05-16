af7a0156f99855f5e24dadad86c7d23e14f0e42f
somnif <teewaard@gmail.com>
2024-05-16 16:57:03 +1200

    Add functionality to redraw grid, and to specify a new grid size.

--- js-file.js
+++ js-file.js
@@ -1,15 +1,19 @@
 const container = document.querySelector("#container")
+const promptButton = document.querySelector("#btn-prompt")
+const resetButton = document.querySelector("#btn-reset")
+
 const INITIAL_SIZE = 16;
 let gridWidth = INITIAL_SIZE;
 let gridHeight = INITIAL_SIZE;
 
-const drawGrid = (width, height) => {
+const drawGrid = () => {
+    console.log("drawing a grid to" + gridHeight)
     container.innerText = "";
-    for (let i = 0; i < height; i++){
+    for (let i = 0; i < gridHeight; i++){
         let row = document.createElement("div")
         row.setAttribute("class", "grid-row")
 
-        for (let j = 0; j < width; j++){
+        for (let j = 0; j < gridWidth; j++){
             let grid = document.createElement("div")
             grid.setAttribute("class", "grid-item")
             row.appendChild(grid)
@@ -21,6 +25,26 @@ const drawGrid = (width, height) => {
     }
 }
 
-window.addEventListener("load", drawGrid(gridWidth, gridHeight), {
+const promptUser = () => {
+    let newSize = parseInt(prompt("Please enter a new grid size"));
+    console.log (newSize)
+    if (Number.isInteger(newSize) && newSize <= 100 && 1 <= newSize){
+        newGrid (newSize)
+    } else {
+        alert("Please enter a number between 1 and 100")
+    }
+}
+
+const newGrid = (size) => {
+    console.log("Resizing to " + size)
+    gridWidth = size;
+    gridHeight = size;
+    drawGrid();
+}
+
+promptButton.addEventListener("click", promptUser)
+resetButton.addEventListener("click", drawGrid)
+
+window.addEventListener("load", drawGrid, {
     once: true
 });
